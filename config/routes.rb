b3536be3c31e532a32e4f6822f2bdbea2c963008
Natacha Ducre <natacha.ducre@gmail.com>
2017-11-23 12:32:28 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,14 @@
 Rails.application.routes.draw do
-  get 'doses/index'
 
-  get 'doses/show'
 
-  get 'doses/new'
+  root to 'cocktails#index'
 
-  get 'doses/create'
 
-  get 'doses/edit'
-
-  get 'doses/update'
-
-  get 'doses/destroy'
-
-  get 'ingredients/index'
-
-  get 'ingredients/new'
-
-  get 'ingredients/create'
-
-  get 'ingredients/destroy'
-
-  get 'cocktails/index'
-
-  get 'cocktails/show'
-
-  get 'cocktails/new'
-
-  get 'cocktails/create'
-
-  get 'cocktails/edit'
-
-  get 'cocktails/update'
-
-  get 'cocktails/destroy'
+   resources :cocktails do
+    resources :ingredients do
+      resources :doses
+      end
+    end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
